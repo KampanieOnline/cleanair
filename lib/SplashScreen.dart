48fc005dd2e9a7d411970e37d31d1b033be4adc7
wrzosdev <wrzosdev@gmail.com>
2021-07-16 21:20:33 +0200

    6.4 Stylowanie ekranu SplashScreen

--- lib/SplashScreen.dart
+++ lib/SplashScreen.dart
@@ -1,5 +1,5 @@
-
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class SplashScreen extends StatefulWidget {
   SplashScreen({Key key, this.title}) : super(key: key);
@@ -11,7 +11,6 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
-
   @override
   Widget build(BuildContext context) {
     // This method is rerun every time setState is called, for instance as done
@@ -21,36 +20,58 @@ class _SplashScreenState extends State<SplashScreen> {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Scaffold(
-      body: Stack(
-        fit: StackFit.expand,
-        children: <Widget>[
-          Container(
-
-          ),
-          Align(
+      body: Stack(fit: StackFit.expand, children: <Widget>[
+        Container(
+          decoration: BoxDecoration(
+              color: new Color(0xffffffff),
+              gradient: LinearGradient(
+                  begin: Alignment.centerRight,
+                  end: Alignment.centerLeft,
+                  colors: [new Color(0xff6671e5), new Color(0xff4852d9)])),
+        ),
+        Align(
             alignment: FractionalOffset.center,
-            child:
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Icon(Icons.wb_cloudy, color: Colors.white),
-                  Padding(padding: EdgeInsets.only(top:15.0)),
-                  Text(widget.title),
-                  Padding(padding: EdgeInsets.only(top:5.0)),
-                  Text('Aplikacja do monitorowania \n czystości powietrza'),
-                ],
-              )
-          ),
-          Positioned(
-              left: 0,
-              bottom: 35,
-              right: 0,
-              child: Container(
-                alignment: Alignment.center,
-            child: Text("Przywiewam dane..."),
-          ))
-        ]
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Image(
+                  image: AssetImage('icons/cloud-sun.png'),
+                ),
+                Padding(padding: EdgeInsets.only(top: 15.0)),
+                Text(widget.title,
+                    textAlign: TextAlign.center,
+                    style: GoogleFonts.lato(
+                        textStyle: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 42.0,
+                      color: Colors.white,
+                    ))),
+                Padding(padding: EdgeInsets.only(top: 5.0)),
+                Text('Aplikacja do monitorowania \n czystości powietrza',
+                    textAlign: TextAlign.center,
+                    style: GoogleFonts.lato(
+                        textStyle: TextStyle(
+                      fontSize: 16.0,
+                      color: Colors.white,
+                    ))),
+              ],
+            )),
+        Positioned(
+            left: 0,
+            bottom: 35,
+            right: 0,
+            child: Container(
+              alignment: Alignment.center,
+              child: Text("Przywiewam dane...",
+                  textAlign: TextAlign.center,
+                  style: GoogleFonts.lato(
+                      textStyle: TextStyle(
+                        fontWeight: FontWeight.w300,
+                    fontSize: 18.0,
+                    color: Colors.white,
+                  ))),
+            ))
+      ]), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
