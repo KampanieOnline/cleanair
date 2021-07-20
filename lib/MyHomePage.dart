372e86e9fcb8f591e70907ebe42ba11cb56a6a8d
wrzosdev <wrzosdev@gmail.com>
2021-07-21 01:50:47 +0200

    7.2 Ustawienie i konfiguracja nawigacji dolnej

--- lib/MyHomePage.dart
+++ lib/MyHomePage.dart
@@ -1,4 +1,5 @@
-
+import 'package:clean_air/AirScreen.dart';
+import 'package:clean_air/WeatherScreen.dart';
 import 'package:flutter/material.dart';
 
 class MyHomePage extends StatefulWidget {
@@ -7,25 +8,27 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  var _currentIndex = 0;
+  final screens = [
+    AirScreen(),
+    WeatherScreen(),
+  ];
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-        body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Text("HOME")
+      body: IndexedStack(
+        index: _currentIndex,
+        children: screens,
       ),
       bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (index) => setState(()=>_currentIndex = index),
         items: [
-          BottomNavigationBarItem(icon: Icon(Icons.masks_outlined), label: "Powietrze"),
-          BottomNavigationBarItem(icon: Icon(Icons.wb_cloudy_outlined), label: "Pogoda")
+          BottomNavigationBarItem(
+              icon: Icon(Icons.masks_outlined), label: "Powietrze"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.wb_cloudy_outlined), label: "Pogoda")
         ],
       ),
     );
---
+++ lib/WeatherScreen.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import 'main.dart';
+
+class WeatherScreen extends StatefulWidget {
+  @override
+  _WeatherScreenState createState() => _WeatherScreenState();
+}
+
+class _WeatherScreenState extends State<WeatherScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(fit: StackFit.expand, children: <Widget>[
+        Container(
+          decoration: BoxDecoration(
+              color: new Color(0xffffffff),
+              gradient: LinearGradient(
+                  begin: Alignment.centerRight,
+                  end: Alignment.centerLeft,
+                  colors: [new Color(0xff6671e5), new Color(0xff4852d9)])),
+        ),
+        Align(
+            alignment: FractionalOffset.center,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Image(
+                  image: AssetImage('icons/cloud-sun.png'),
+                ),
+                Padding(padding: EdgeInsets.only(top: 15.0)),
+                Text(Strings.appTitle,
+                    textAlign: TextAlign.center,
+                    style: GoogleFonts.lato(
+                        textStyle: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 42.0,
+                      color: Colors.white,
+                    ))),
+                Padding(padding: EdgeInsets.only(top: 5.0)),
+                Text('Pogoda',
+                    textAlign: TextAlign.center,
+                    style: GoogleFonts.lato(
+                        textStyle: TextStyle(
+                      fontSize: 16.0,
+                      color: Colors.white,
+                    ))),
+              ],
+            )),
+        Positioned(
+            left: 0,
+            bottom: 35,
+            right: 0,
+            child: Container(
+              alignment: Alignment.center,
+              child: Text("Przywiewam dane...",
+                  textAlign: TextAlign.center,
+                  style: GoogleFonts.lato(
+                      textStyle: TextStyle(
+                    fontWeight: FontWeight.w300,
+                    fontSize: 18.0,
+                    color: Colors.white,
+                  ))),
+            ))
+      ]), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  bool havePermissionToAsk() {
+    return true;
+  }
+}
