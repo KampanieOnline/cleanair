f7d745815b3baa8f492353ec77683eb702e9e8ff
wrzosdev <wrzosdev@gmail.com>
2021-08-25 02:10:16 +0200

    9.1 Ramowy rozkład elementów na ekranie stanu powietrza

--- lib/AirScreen.dart
+++ lib/AirScreen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -26,43 +27,153 @@ class _AirScreenState extends State<AirScreen> {
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
-                Image(
-                  image: AssetImage('icons/cloud-sun.png'),
-                  color: Colors.black,
+                Text("Jakość powietrza"),
+                Padding(padding: EdgeInsets.only(top: 2)),
+                Text("Bardzo dobra"),
+                Padding(padding: EdgeInsets.only(top: 24)),
+                CircleAvatar(
+                  radius: 91.0,
+                  child: Center(
+                    child: Container(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text("23",
+                            textAlign: TextAlign.center,),
+                          Text("CAQI ⓘ",
+                            textAlign: TextAlign.center,),
+                        ],
+                      )
+                    ),
+                  ),
                 ),
-                Padding(padding: EdgeInsets.only(top: 15.0)),
-                Text(Strings.appTitle,
-                    textAlign: TextAlign.center,
-                    style: GoogleFonts.lato(
-                        textStyle: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 42.0,
-                      color: Colors.black,
-                    ))),
-                Padding(padding: EdgeInsets.only(top: 5.0)),
-                Text('Powietrze',
-                    textAlign: TextAlign.center,
-                    style: GoogleFonts.lato(
-                        textStyle: TextStyle(
-                      fontSize: 16.0,
-                      color: Colors.black,
-                    ))),
+                Padding(padding: EdgeInsets.only(top: 28)),
+                IntrinsicHeight(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Container(
+                          width: 130,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: [
+                              Text(
+                                "PM 2,5",
+                                textAlign: TextAlign.center,
+                                style: GoogleFonts.lato(
+                                    textStyle: TextStyle(
+                                        fontSize: 14.0,
+                                        height: 1.2,
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.w300)),
+                              ),
+                              Padding(padding: EdgeInsets.only(top: 2.0)),
+                              Text(
+                                "10%",
+                                textAlign: TextAlign.center,
+                                style: GoogleFonts.lato(
+                                    textStyle: TextStyle(
+                                        fontSize: 26.0,
+                                        height: 1.2,
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.w700)),
+                              ),
+                            ],
+                          ),
+                        ),
+                        VerticalDivider(
+                          width: 48,
+                          color: Colors.white,
+                          thickness: 1,
+                        ),
+                        Container(
+                          width: 130,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              Text("PM 10",
+                                  textAlign: TextAlign.center,
+                                  style: GoogleFonts.lato(
+                                      textStyle: TextStyle(
+                                          fontSize: 14.0,
+                                          height: 1.2,
+                                          color: Colors.white,
+                                          fontWeight: FontWeight.w300))),
+                              Padding(padding: EdgeInsets.only(top: 2.0)),
+                              Text(
+                                "12%",
+                                textAlign: TextAlign.center,
+                                style: GoogleFonts.lato(
+                                    textStyle: TextStyle(
+                                        fontSize: 26.0,
+                                        height: 1.2,
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.w700)),
+                              ),
+                            ],
+                          ),
+                        )
+                      ],
+                    )),
+                Padding(padding: EdgeInsets.only(top: 20)),
+                Text("Stacja pomiarowa"),
+                Padding(padding: EdgeInsets.only(top: 8)),
+                Text("Warszawa"),
+
               ],
             )),
         Positioned(
-            left: 0,
-            bottom: 35,
+            left: 8,
+            bottom: 0,
             right: 0,
+            top: 0,
+            child: Container(
+              alignment: Alignment.centerLeft,
+              child: Stack(children: [
+                ClipRect(
+                  child: Align(
+                    alignment: Alignment.topLeft,
+                    heightFactor: 1,
+                      child: Image.asset('icons/danger-value-negative.png', scale: 0.9),
+                  ),
+                ),
+                ClipRect(
+                  child: Align(
+                    alignment: Alignment.topLeft,
+                    heightFactor: 1-0.4,
+                    child: Image.asset('icons/danger-value.png', scale: 0.9),
+                  ),
+                )
+              ],),
+            )),
+        Positioned(
+            left: 0,
+            right: 0,
+            bottom: 0,
             child: Container(
               alignment: Alignment.center,
-              child: Text("Przywiewam dane...",
-                  textAlign: TextAlign.center,
-                  style: GoogleFonts.lato(
-                      textStyle: TextStyle(
-                    fontWeight: FontWeight.w300,
-                    fontSize: 18.0,
-                    color: Colors.black,
-                  ))),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Padding(padding: EdgeInsets.only(
+                    top:62.0, left:10, right: 10, bottom: 14),
+                    child: Divider(
+                        height: 10,
+                        color: Colors.black,
+                        thickness: 1,
+                      )),
+                        Padding(padding: EdgeInsets.only(
+     left:10, right: 10, bottom: 24),
+    child: ClipRRect(
+      borderRadius: BorderRadius.circular(6.0),
+      child: Container(
+        color: Colors.white,
+        child: Text("Wyjdź na dwór")
+      ),
+    )
+                        ),
+                ],
+              ),
             ))
       ]), // This trailing comma makes auto-formatting nicer for build methods.
     );
